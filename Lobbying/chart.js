ef91c1332a714040c68985b9c0f9bceba757ca12
NikosPi <nickospi@gmail.com>
2018-08-21 08:43:55 -0400

    f

--- Lobbying/chart.js
+++ Lobbying/chart.js
@@ -18,8 +18,8 @@
 
 
   var simulation = d3.forceSimulation()
-    .force("x", d3.forceX(width ).strength(0.05))
-    .force("y", d3.forceY(height ).strength(0.05))
+    .force("x", d3.forceX(width/2 ).strength(0.15))
+    .force("y", d3.forceY(height/2 ).strength(0.15))
     .force("collide", d3.forceCollide(function(d){
       return radiusScale(d.lobbying_costs) + 1;
     
