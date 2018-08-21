0267e1c69f79a7fedee18df657548d4155f2b7ca
NikosPi <nickospi@gmail.com>
2018-08-21 14:49:12 -0400

    f

--- Lobbying/chart.js
+++ Lobbying/chart.js
@@ -42,7 +42,7 @@
     .style("visibility", "hidden")
     .style("background", "#ffffff")
     .text("a simple tooltip")
-    .style('font-family', "Roboto")
+    .style('font-family', '"Roboto", sans-serif')
  
 
 
@@ -116,8 +116,11 @@
       .attr("fill", "#ffffff")
       .attr('textLength',"50")
       .attr('lengthAdjust',"spacingAndGlyphs")
-      .style('font-family', "Roboto")
+      .style('font-family', '"Roboto", sans-serif')
       .style('font-size', '12px')
+
+
+
 
       var $window = $(window);
       var $elem = $(".animation")
